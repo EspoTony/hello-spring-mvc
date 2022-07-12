28c4aa9510132e8b3bee54464c6723f809c5c4e0
EspoTony <espotony98@gmail.com>
2022-07-12 14:48:17 +0200

    esercizio base completato,forse

---
+++ src/main/java/jana60Controller/MainController.java
@@ -0,0 +1,16 @@
+package jana60Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/")
+public class MainController {
+	@GetMapping("/home")
+	public String home() {
+		return "home";
+	}
+
+}
